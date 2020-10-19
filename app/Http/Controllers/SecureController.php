7e5ef64d407549049b43351a137bb47db305b312
Acip <freshbone.acip@yahoo.co.id>
2020-10-19 13:33:32 +0700

    fixed UserController, SecureController, User model, AuthServiceProvider, and Authenticate middleware

--- app/Http/Controllers/SecureController.php
+++ app/Http/Controllers/SecureController.php
@@ -2,11 +2,15 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class SecureController extends Controller
 {
 	public function profile(Request $request)
 	{
-		
+        return response()->json([
+            'status' => true,
+            'data' => Auth::user()
+        ]);
 	}
 }
--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -1,17 +1,42 @@
 <?php
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
 
 class UserController extends Controller
 {
     public function index(Request $request)
     {
-        
+        // check "with" parameter
+        $relations = $request->query('with');
+        $with_arr = [];
+        foreach (explode(',', $relations) AS $with) {
+            if ($with = trim($with)) {
+                // check if relation exists
+                if (!method_exists(User::class, $with)) {
+                    return response()->json([
+                        'status' => false,
+                        'message' => 'Undefined relation [' . $with . ']'
+                    ], 403);
+                } else {
+                    $with_arr[] = $with;
+                }
+            }
+        }
+        // get user data
+        $users = User::with($with_arr)->get();
+        return response()->json([
+            'status' => true,
+            'data' => $users
+        ]);
     }
 
     public function view(Request $request, string $user)
     {
-
+        return response()->json([
+            'status' => true,
+            'data' => User::where('id', $user)->first()
+        ]);
     }
 }
